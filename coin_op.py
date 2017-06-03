a383f386745938eb24aa6929f149f8a4faca2ba6
wtesler <willtesler@gmail.com>
2017-06-03 03:42:58 -0700

    Simplify.

--- coin_op.py
+++ coin_op.py
@@ -1,5 +1,10 @@
 __author__ = 'Will Tesler'
 
+from datetime import datetime
+import json
+import pyrebase
+from coinmarketcap import Market
+
 # Firebase Config
 config = {
     "apiKey": "FIREBASE_WEB_API_KEY",
@@ -9,59 +14,61 @@ config = {
     "serviceAccount": "firebase-service-account.json"
 }
 
-COIN_LIMIT = "10"
-
-from datetime import datetime
-import json
-import pyrebase
-from coinmarketcap import Market
-
+GLOBAL_STATS = "global_stats"
+TIMESTAMPS = "timestamps"
+TOP_X_COINS = 10
 
 def run(event, context):
+    # The Database.
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
 
+    # The id associated with this particular run of the function.
+    stepUuid = db.generate_key()
+
+    # Current UTC Time
+    timestamp = datetime.utcnow().strftime("%Y %m %d %H %M %S")
+
+    # The market has coin data in it.
     market = Market()
 
-    data = {}
-    timestamp = datetime.utcnow().strftime("%Y %m %d %H %M %S")
-    uuid = db.generate_key()
+    # Accumulate data in this update from the coin and global data.
+    update = {}
 
-    key_timestamp = "timestamps"
-    key_total_24h_volume_usd = "total_24h_volume_usd"
-    key_total_market_cap_usd = "total_market_cap_usd"
+    _addCoinData(market, update, stepUuid, timestamp)
+    _addGlobalData(market, update, stepUuid, timestamp)
 
-    global_stats = json.loads(market.stats())
+    db.update(update)
 
-    setGlobalData(key_timestamp, uuid, timestamp, data)
-    setGlobalData(key_total_24h_volume_usd, uuid, global_stats[key_total_24h_volume_usd], data)
-    setGlobalData(key_total_market_cap_usd, uuid, global_stats[key_total_market_cap_usd], data)
 
-    coin_stats = json.loads(market.ticker("?limit=" + COIN_LIMIT))
-
-    key_price_usd = "price_usd"
-    key_percent_change_1h = "percent_change_1h"
-    key_percent_change_24h = "percent_change_24h"
-    key_percent_change_7d = "percent_change_7d"
-    key_24h_volume_usd = "24h_volume_usd"
-    key_market_cap_usd = "market_cap_usd"
-
+# Add new coin data from the market to our accumulating data.
+def _addCoinData(market, data, stepUuid, timestamp):
+    coin_stats = json.loads(market.ticker("?limit=" + str(TOP_X_COINS)))
     for coin in coin_stats:
-        coin_id = coin["id"]
-        setCoinData(coin_id, key_timestamp, uuid, timestamp, data)
-        setCoinData(coin_id, key_price_usd, uuid, coin[key_price_usd], data)
-        setCoinData(coin_id, key_percent_change_1h, uuid, coin[key_percent_change_1h], data)
-        setCoinData(coin_id, key_percent_change_24h, uuid, coin[key_percent_change_24h], data)
-        setCoinData(coin_id, key_percent_change_7d, uuid, coin[key_percent_change_7d], data)
-        setCoinData(coin_id, key_24h_volume_usd, uuid, coin[key_24h_volume_usd], data)
-        setCoinData(coin_id, key_market_cap_usd, uuid, coin[key_market_cap_usd], data)
-
-    db.update(data)
+        _setTimestamp(coin["id"], stepUuid, timestamp, data)
+        _setCoinData(coin, "price_usd", stepUuid, data)
+        _setCoinData(coin, "percent_change_1h", stepUuid, data)
+        _setCoinData(coin, "percent_change_24h", stepUuid, data)
+        _setCoinData(coin, "percent_change_7d", stepUuid, data)
+        _setCoinData(coin, "24h_volume_usd", stepUuid, data)
+        _setCoinData(coin, "market_cap_usd", stepUuid, data)
 
 
-def setGlobalData(key, uuid, value, data):
-    data["global_stats" + "/" + key + "/" + uuid] = value
+# Add new global data from the market to our accumulating data.
+def _addGlobalData(market, data, stepUuid, timestamp):
+    global_stats = json.loads(market.stats())
+    _setTimestamp(GLOBAL_STATS, stepUuid, timestamp, data)
+    _setGlobalData("total_24h_volume_usd", stepUuid, global_stats, data)
+    _setGlobalData("total_market_cap_usd", stepUuid, global_stats, data)
 
 
-def setCoinData(coinId, key, uuid, value, data):
-    data[coinId + "/" + key + "/" + uuid] = value
+def _setTimestamp(prefix, uuid, value, data):
+    data[prefix + "/" + TIMESTAMPS + "/" + uuid] = value
+
+
+def _setCoinData(coin, key, uuid, data):
+    data[coin["id"] + "/" + key + "/" + uuid] = coin[key]
+
+
+def _setGlobalData(key, uuid, global_stats, data):
+    data[GLOBAL_STATS + "/" + key + "/" + uuid] = global_stats[key]
